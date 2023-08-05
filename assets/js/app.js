fffecca1dc3c0ad005237ecfe514be3417ed7ab7
Yassine Ibenbrahim <90098755+yassineibenbrahim@users.noreply.github.com>
2023-08-05 13:22:17 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -169,7 +169,7 @@ let footer = $(`
 
 
 
-    <a class="social-button instagram" href="https://www.instagram.com/limbslap/" target="_blank" >
+    <a class="social-button instagram" href="https://www.instagram.com/yassineibenb/" target="_blank" >
     <!-- svg code for instagram icon -->
       <svg class="instagram-icon-footer" x="0px" y="0px" viewBox="0 0 202.5 202.5"
         style="enable-background:new 0 0 202.5 202.5;">
